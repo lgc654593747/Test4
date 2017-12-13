c052e28eca4ed37adb859e81f8fd6ba058775eee
Administrator <Administrator@C7U4KJA9GY48LUC>
2017-12-13 08:56:29 +0800

    first commit

---
+++ Test4/src/cn/test/Test4.java
@@ -0,0 +1,20 @@
+package cn.test;
+
+public class Test4 {
+     private int username;
+     private int password;
+	public int getUsername() {
+		return username;
+	}
+	public void setUsername(int username) {
+		this.username = username;
+	}
+	public int getPassword() {
+		return password;
+	}
+	public void setPassword(int password) {
+		this.password = password;
+	}
+     
+     
+}
